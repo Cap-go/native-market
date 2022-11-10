08e44b359b312e1c98110e13447263dbc5721fc3
martindonadieu <martindonadieu@gmail.com>
2022-11-10 21:11:07 +0800

    fix: app store appID

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,6 +1,24 @@
 import Foundation
 import Capacitor
 
+struct APIResult: Codable {
+    struct App: Codable {
+        let trackId: Int
+
+        enum CodingKeys : String, CodingKey {
+            case trackId
+        }
+    }
+
+    let resultCount: Int
+    let apps: [App]
+
+    enum CodingKeys : String, CodingKey {
+        case resultCount
+        case apps = "results"
+    }
+}
+
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
@@ -12,20 +30,29 @@ public class NativeMarket: CAPPlugin {
         if call.hasOption("appId") {
             let appId = call.getString("appId")
             
-            let url = "itms-apps://itunes.apple.com/app/" + appId!
-            let appUrl = URL(string: url)
-            
-            DispatchQueue.main.async {
-                if UIApplication.shared.canOpenURL(appUrl!) {
-                    if #available(iOS 10.0, *) {
-                        UIApplication.shared.open(appUrl!, options: [:]) { (success) in
+            do {
+                let url = URL(string: "http://itunes.apple.com/lookup?bundleId=\(appId)")
+                let data = try Data(contentsOf: url!)
+                let decoder = JSONDecoder()
+                let apps = try! decoder.decode(APIResult.self, from: data).apps
+                let urlStore = "itms-apps://itunes.apple.com/app/id\(apps[0].trackId)"
+                let appUrl = URL(string: urlStore)
+                
+                DispatchQueue.main.async {
+                    if UIApplication.shared.canOpenURL(appUrl!) {
+                        if #available(iOS 10.0, *) {
+                            UIApplication.shared.open(appUrl!, options: [:]) { (success) in
+                                call.resolve()
+                            }
+                        } else {
+                            UIApplication.shared.openURL(appUrl!)
                             call.resolve()
                         }
-                    } else {
-                        UIApplication.shared.openURL(appUrl!)
-                        call.resolve()
                     }
                 }
+            } catch {
+                print("trackId cannot be found from appId")
+                call.reject("trackId cannot be found from appId")
             }
         } else {
             call.reject("appId is missing")
