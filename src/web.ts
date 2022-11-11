e7ccc1faad0365faa9e841b035b070065ffbbae3
martindonadieu <martindonadieu@gmail.com>
2022-11-11 15:07:32 +0800

    fix: lint issue

--- src/web.ts
+++ src/web.ts
@@ -1,24 +1,25 @@
-import { WebPlugin } from "@capacitor/core";
-import { NativeMarketPlugin } from "./definitions";
+import { WebPlugin } from '@capacitor/core';
+
+import type { NativeMarketPlugin } from './definitions';
 
 export class NativeMarketWeb extends WebPlugin implements NativeMarketPlugin {
   openStoreListing(_options: { appId: string }): Promise<void> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
 
   openDevPage(_options: { devId: string }): Promise<void> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
 
   openCollection(_options: { name: string }): Promise<void> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
 
   openEditorChoicePage(_options: { editorChoice: string }): Promise<void> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
 
   search(_options: { terms: string }): Promise<void> {
-    throw new Error("Method not implemented.");
+    throw new Error('Method not implemented.');
   }
 }
